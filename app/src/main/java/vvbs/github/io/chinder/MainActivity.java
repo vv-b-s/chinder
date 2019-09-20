c958aaa4099f8838a91412fe9dbed312a10f1da2
valentin <valentin.v1997@gmail.com>
2019-09-20 22:41:03 +0300

    initial commit

---
+++ app/src/main/java/vvbs/github/io/chinder/MainActivity.java
@@ -0,0 +1,14 @@
+package vvbs.github.io.chinder;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
